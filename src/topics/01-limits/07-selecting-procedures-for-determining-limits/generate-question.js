2124826096ac2db25e3aaa51679410d58f815a8e
simbag04 <124486722+simbag04@users.noreply.github.com>
2023-10-12 00:38:54 -0700

    Add generate question for selecting procedures

---
+++ src/topics/01-limits/07-selecting-procedures-for-determining-limits/generate-question.js
@@ -0,0 +1,26 @@
+import g02 from "../02-evaluating-limits-using-graphs/generate-question";
+import g03 from "../03-estimating-limits-using-tables/generate-question";
+import g04 from "../04-evaluating-limits-using-algebraic-properties/generate-question";
+import g05 from "../05-evaluating-limits-using-direct-substitution/generate-question";
+import g06 from "../06-evaluating-limits-using-algebraic-manipulation/generate-question";
+import { getRandomNumber } from "../../../helpers/functions";
+
+function generateRandomQuestion() {
+  const rand = getRandomNumber(1, 10);
+  let q = null;
+  if (rand <= 1) {
+    q = g02();
+  } else if (rand <= 2) {
+    q = g03();
+  } else if (rand <= 4) {
+    q = g04();
+  } else if (rand <= 7) {
+    q = g05();
+  } else {
+    q = g06();
+  }
+
+  return q;
+}
+
+export default generateRandomQuestion
