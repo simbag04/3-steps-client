5f97dd2ee8ac11d55a85d0a8dbb26a5446e3a83a
simbag04 <124486722+simbag04@users.noreply.github.com>
2023-10-20 23:53:50 -0700

    Finish learn content for next topic

--- src/topics/01-limits/08-determining-limits-using-the-squeeze-theorem/Sections.js
+++ src/topics/01-limits/08-determining-limits-using-the-squeeze-theorem/Sections.js
@@ -9,6 +9,30 @@ const n2 = math.parse('x^2');
 const n3 = math.parse('-x^2');
 const functions = [
   {
+    f: x => n2.evaluate({ x }),
+    min: -0.5,
+    max: 0.5,
+    includeLeft: false,
+    includeRight: false,
+    leftArrow: true,
+    rightArrow: true,
+    classes: 'c3',
+    leftCircle: false,
+    rightCircle: false,
+  },
+  {
+    f: x => n3.evaluate({ x }),
+    min: -0.5,
+    max: 0.5,
+    includeLeft: false,
+    includeRight: false,
+    leftArrow: true,
+    rightArrow: true,
+    classes: 'c1',
+    leftCircle: false,
+    rightCircle: false,
+  },
+  {
     f: x => n1.evaluate({ x }),
     min: -0.5,
     max: 0,
@@ -34,30 +58,6 @@ const functions = [
     rightCircle: false,
     dataGap: 0.001
   },
-  {
-    f: x => n2.evaluate({ x }),
-    min: -0.5,
-    max: 0.5,
-    includeLeft: false,
-    includeRight: false,
-    leftArrow: true,
-    rightArrow: true,
-    classes: 'c1',
-    leftCircle: false,
-    rightCircle: false,
-  },
-  {
-    f: x => n3.evaluate({ x }),
-    min: -0.5,
-    max: 0.5,
-    includeLeft: false,
-    includeRight: false,
-    leftArrow: true,
-    rightArrow: true,
-    classes: 'c3',
-    leftCircle: false,
-    rightCircle: false,
-  },
 ]
 
 const Section1 = <>
@@ -65,13 +65,116 @@ const Section1 = <>
     When calculating limits, one theorem that is useful is the <strong>Squeeze Theorem</strong>.
   </div>
   <div>
-    This theorem says that <strong>if</strong> <Latex classes={'c1'} expression={`f(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c2'} expression={`g(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c3'} expression={`h(x)`} /> <strong>near</strong> <Latex expression={`x = a`} /> (but not necessarily <strong><em>at</em></strong> <Latex expression={`x = a`} />), and <Latex expression={`\\\\\\lim_{x \\to a}`} /><Latex classes={'c1'} expression={`f(x)`} /> <Latex expression={`=\\lim_{x \\to a}`} /> <Latex classes={'c3'} expression={`h(x)`} /> <Latex expression={`=L`} />, then <Latex expression={`\\lim_{x \\to a}`} /> <Latex classes={'c2'} expression={`g(x)`} /> <Latex expression={`=L`} />.
+    This theorem says that <strong>if</strong> <Latex classes={'c1'} expression={`f(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c2'} expression={`g(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c3'} expression={`h(x)`} /> <strong>near</strong> <Latex expression={`x = a`} /> (but not necessarily <strong><em>at</em></strong> <Latex expression={`x = a`} />),
+  </div>
+  <div>
+    and <Latex classes={'c1'} expression={`\\lim_{x \\to a} f(x)`} /> <Latex expression={`=`} /> <Latex classes={'c2'} expression={`\\lim_{x \\to a} h(x)`} /> <Latex classes={'c4'} expression={'=L'} />, then <Latex classes={'c3'} expression={`\\lim_{x \\to a} g(x)`} /> <Latex classes={'c4'} expression={'=L'} />.
   </div>
 </>
 
 const Section2 = <>
-  To understand this, let's look at an example graph.
-  <FunctionGraph functions={functions} size={GRAPH_SIZE} minx={-0.3} maxx={0.3} miny={-0.3} maxy = {0.3} />
+  <div>
+    To understand this, let's look at an example graph with <Latex classes={'c1'} expression={`f(x) = -x^2`} />, <Latex classes={'c2'} expression={`g(x) = x^2 \\sin(\\frac{1}{x})`} />, <Latex classes={'c3'} expression={`h(x) = x^2`} />
+  </div>
+  <div className="flex vertical center medium-gap">
+    <h3>Sample Graph</h3>
+    <FunctionGraph functions={functions} size={GRAPH_SIZE} minx={-0.3} maxx={0.3} miny={-0.3} maxy={0.3} />
+  </div>
 </>
 
-export { Section1, Section2 }
+const Section3 = <>
+  <div>
+    We want to evaluate <Latex expression={`\\lim_{x \\to 0}`} /> <Latex classes={'c2'} expression={`g(x)`} />. However, using direct substition, we get: <Latex expression={`\\lim_{x \\to 0} g(x) = \\lim_{x \\to 0} x^2 \\sin(\\frac{1}{x}) = (0)^2 \\sin(\\frac{1}{0})`} display={true} />
+  </div>
+  <div>
+    Graphically, it is clear that this limit is <Latex expression={`0`} />. But how can we evaluate this limit analytically?
+  </div>
+</>
+
+const Section4 = <>
+  <div>
+    We can use the Squeeze Theorem! In this example, we see that <Latex classes={'c1'} expression={`f(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c2'} expression={`g(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c3'} expression={`h(x)`} />
+  </div>
+</>
+
+const Section5 = <>
+  <div>
+    We can focus on the limits of the top and bottom functions:
+    <Latex classes={'c1'} expression={`\\lim_{x \\to 0} f(x)= \\lim_{x \\to 0} -x^2 = 0`} display={true} />
+    <Latex classes={'c3'} expression={`\\lim_{x \\to 0} h(x)= \\lim_{x \\to 0} x^2 = 0`} display={true} />
+  </div>
+</>
+
+const Section6 = <>
+  <div>
+    Since <Latex classes={'c1'} expression={`f(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c2'} expression={`g(x)`} /> <Latex expression={`\\leq`} /> <Latex classes={'c3'} expression={`h(x)`} /> near <Latex expression={`x = 0`} />, and <Latex classes={'c1'} expression={`\\lim_{x \\to 0} f(x)=0`} />, and <Latex classes={'c3'} expression={`\\lim_{x \\to 0} h(x)=0`} />,
+  </div>
+  <div>
+    by the Squeeze Theorem, <Latex classes={'c2'} expression={`\\lim_{x \\to 0} g(x)=0`} />
+  </div>
+</>
+
+const Section7 = <>
+  <div>
+    However, how could we have known that <Latex classes={'c1'} expression={`-x^2`} /> <Latex expression={`\\leq`} /> <Latex classes={'c2'} expression={`x^2 \\sin(\\frac{1}{x})`} /> <Latex expression={`\\leq`} /> <Latex classes={'c3'} expression={`x^2`} /> without looking at the graph?
+  </div>
+</>
+
+const Section8 = <>
+  <div>
+    Since we want to find <Latex classes={'c2'} expression={`\\lim_{x \\to 0} x^2 \\sin(\\frac{1}{x})`} />, first we can focus on just <Latex expression={`\\sin(\\frac{1}{x})`} />.
+  </div>
+  <div>
+    Since this is a <Latex expression={`\\sin`} /> function, we know that <Latex expression={`-1 \\leq \\sin(\\frac{1}{x}) \\leq 1`} />
+  </div>
+</>
+
+const Section9 = <>
+  <div>
+    Now, multiplying the whole inequality by <Latex expression={`x^2`} />, we get <Latex expression={`-x^2 \\leq x^2\\sin(\\frac{1}{x}) \\leq x^2`} />, which is exactly what we want!
+  </div>
+</>
+
+const Section10 = <>
+  <div>
+    Lastly, we just take <Latex expression={`\\lim_{x \\to 0}`} /> of all 3 expressions, which gives us:
+    <Latex expression={`\\lim_{x \\to 0}-x^2 \\leq \\lim_{x \\to 0}x^2\\sin(\\frac{1}{x}) \\leq \\lim_{x \\to 0} x^2`} display={true} />
+    <Latex expression={`0 \\leq \\lim_{x \\to 0}x^2\\sin(\\frac{1}{x}) \\leq 0`} display={true} />
+    <Latex expression={`\\lim_{x \\to 0}x^2\\sin(\\frac{1}{x}) = 0`} display={true} />
+  </div>
+  <div>
+    This is the same as the result from before!
+  </div>
+</>
+
+const Section11 = <>
+  <div>
+    The Squeeze Theorem can also be used to show that <Latex expression={`\\lim_{x \\to 0} \\frac{\\sin x}{x} = 1`} /> and <Latex expression={`\\lim_{x \\to 0} \\frac{1 - \\cos x}{x} = 0`} />
+  </div>
+  <div>
+    These can be used to evaluate limits very easily!
+  </div>
+</>
+
+const Section12 = <>
+  <div>
+    For example, what is <Latex expression={`\\lim_{x \\to 0} \\frac{\\sin(7x)}{x}`} />?
+  </div>
+</>
+
+const Section13 = <>
+  <div>
+    Multiplying the fraction by <Latex expression={`\\frac{7}{7}`} />, <Latex expression={`\\lim_{x \\to 0} \\frac{\\sin(7x)}{x} = \\lim_{x \\to 0} \\frac{7\\sin(7x)}{7x} = 7 \\lim_{x \\to 0} \\frac{\\sin(7x)}{7x}`} display={true} />
+  </div>
+</>
+
+const Section14 = <>
+  <div>
+    Now, we can apply the rule that <Latex expression={`\\lim_{x \\to 0} \\frac{\\sin x}{x} = 0`} /> to say that <Latex expression={`\\lim_{x \\to 0} \\frac{\\sin(7x)}{7x} = 1`} />
+  </div>
+  <div>
+    Notice how it is not necessary that we have <Latex expression={`\\frac{\\sin(x)}{x}`} /> - as long as we have the same expression for both <Latex expression={`x`} /> in <Latex expression={`\\frac{\\sin(x)}{x}`} />, we can use this property.
+  </div>
+</>
+
+export { Section1, Section2, Section3, Section4, Section5, Section6, Section7, Section8, Section9, Section10, Section11, Section12, Section13, Section14 }
--- src/topics/01-limits/08-determining-limits-using-the-squeeze-theorem/learn-content.js
+++ src/topics/01-limits/08-determining-limits-using-the-squeeze-theorem/learn-content.js
@@ -1,8 +1,20 @@
-import { Section1, Section2 } from "./Sections"
+import { Section1, Section10, Section11, Section12, Section13, Section14, Section2, Section3, Section4, Section5, Section6, Section7, Section8, Section9 } from "./Sections"
 
 const learnContent = [
   Section1,
-  Section2
+  Section2,
+  Section3,
+  Section4,
+  Section5,
+  Section6,
+  Section7,
+  Section8,
+  Section9,
+  Section10,
+  Section11,
+  Section12,
+  Section13,
+  Section14
 ]
 
 export default learnContent
